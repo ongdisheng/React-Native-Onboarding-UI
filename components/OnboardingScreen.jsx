a73de9a883abca58819dbdc1bea528a15369ebc0
disheng <ongdisheng@gmail.com>
2023-11-30 07:24:56 +0800

    Added initial components

---
+++ components/OnboardingScreen.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const OnboardingScreen = () => {
+  return (
+    <View>
+      <Text>Onboarding</Text>
+    </View>
+  )
+}
+
+export default OnboardingScreen;
