f2da748e62199db1e9e4d8375aa1c0c68b117726
disheng <ongdisheng@gmail.com>
2023-11-30 18:50:37 +0800

    Updated onboarding component

--- components/OnboardingScreen.jsx
+++ components/OnboardingScreen.jsx
@@ -1,9 +1,62 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity } from "react-native";
+import React from 'react';
+import Onboarding from 'react-native-onboarding-swiper';
+import Lottie from 'lottie-react-native';
+
+const { width } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  lottie:{
+    width: width * 0.9,
+    height: width
+  },
+  doneButton: {
+    padding: 20
+  }
+})
 
 const OnboardingScreen = () => {
   return (
-    <View>
-      <Text>Onboarding</Text>
+    <View style={styles.container}>
+      <Onboarding
+        containerStyles={{paddingHorizontal: 15}}
+        pages={[
+          {
+            backgroundColor: '#a7f3d0',
+            image: (
+              <View style={styles.lottie}>
+                <Lottie source={require('../assets/animations/boost.json')} autoPlay loop />
+              </View>
+            ),
+            title: 'Boost Productivity',
+            subtitle: 'Subscribe this channel to boost your productivity level',
+          },
+          {
+            backgroundColor: '#fef3c7',
+            image: (
+              <View style={styles.lottie}>
+                <Lottie source={require('../assets/animations/work.json')} autoPlay loop />
+              </View>
+            ),
+            title: 'Work Seamlessly',
+            subtitle: 'Get your work done seamlessly without interruption',
+          },
+          {
+            backgroundColor: '#a78bfa',
+            image: (
+              <View style={styles.lottie}>
+                <Lottie source={require('../assets/animations/achieve.json')} autoPlay loop />
+              </View>
+            ),
+            title: 'Achieve Higher Goals',
+            subtitle: 'By boosting your productivity we help you to achieve higher goals',
+          },
+        ]}
+      />
     </View>
   )
 }
